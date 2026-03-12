be6a11daf3f6ca93d4772ec1f0c6e1c54c7d41f5
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2026-03-12 20:46:08 +0700

    Newversion

--- obj/Debug/net8.0/test.AssemblyInfo.cs
+++ obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca7c633dabee3d9b283fe9c219a783009202f28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89c093f950c71be32b68440e643e88ccdde2ae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
